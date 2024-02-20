0644cce878cefa273a6b5b3ca5fdde96d76e8dd8
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-02-20 22:49:09 +0800

    Add files via upload

--- ProjectDesign/php/school_dashboard.php
+++ ProjectDesign/php/school_dashboard.php
@@ -6,6 +6,10 @@
     <title> Responsiive Admin Dashboard | CodingLab </title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <link rel="stylesheet" type="text/css" href="../css/school_dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/school_dashboard.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet '>
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
@@ -85,9 +89,91 @@
   
     
     <div class="home-content">
+        <div class="overview-boxes"> 
+          <div class="box" style="background-color: aquamarine;">
+            <div class="box-topic">STRAND</div>
+          </div>
+        </div>
 
-    <div class="overview-boxes">
-      <div class="box" style="background-color: aquamarine;">
+      <div class="overview-boxes">
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">HUMMS</div>
+            <div class="number">100</div>
+            <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div>
+          </div>
+          <!-- <i class='bx bx-cart-alt cart'></i> -->
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">GAS</div>
+            <div class="number">150</div>
+            <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div>
+          </div>
+          <!-- <i class='bx bxs-cart-add cart two' ></i> -->
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">STEM</div>
+            <div class="number">160</div>
+            <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div>
+          </div>
+          <!-- <i class='bx bx-cart cart three' ></i> -->
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">TECHVOC</div>
+            <div class="number">170</div>
+            <div class="indicator">
+              <i class='bx bx-down-arrow-alt down'></i>
+              <span class="text">Down From Today</span>
+            </div>
+          </div>
+          <!-- <i class='bx bxs-cart-download cart four' ></i> -->
+        </div>
+      </div>
+      
+
+      <div class="sales-boxes">
+        <div class="recent-sales box">
+          <div class="title">Popularity Company </div>
+          <div id="chart_div"></div>
+          <div class="button">
+            <a href="#">See All</a>
+          </div>
+        </div>
+        <div class="top-sales box">
+          <div class="title">Top Company </div>
+          <ul class="top-sales-details">
+            <li>
+            <a href="#">
+              <img src="" alt="">
+              <span class="product">NIA</span>
+            </a>
+            <span class="price">100%</span>
+          </li>
+          <li>
+            <a href="#">
+               <img src="" alt="">
+              <span class="product">Jollibee </span>
+            </a>
+            <span class="price">90%</span>
+          </li>         
+          </ul>
+        </div>
+      </div>
+        
+      <div class="overview-boxes">
+      <div class="box" style="background-color: aquamarine; margin-top:20px;">
         <div class="box-topic">SCHOOL</div>
       </div>
     </div>
@@ -102,9 +188,9 @@
               <span class="text">Up from yesterday</span> -->
             </div>
           </div>
-          <i class='bx bx-cart-alt cart' style="margin-top: 20px; margin-left: 20px;"></i>
+          <!-- <i class='fas fa-user-friends' style="margin-top: 20px; margin-left: 20px;"></i> -->
         </div>
-        <div class="box" style="margin-right:250px;">
+        <div class="box" style="margin-right:250px; ">
           <div class="right-side">
             <div class="box-topic">Partners Industries</div>
             <div class="number">38,876</div>
@@ -113,179 +199,8 @@
               <span class="text">Up from yesterday</span> -->
             </div>
           </div>
-          <i class='bx bxs-cart-add cart two' style="margin-top: 20px; margin-left: 20px;" ></i>
+          <!-- <i class='bx bxs-cart-add cart two' style="margin-top: 20px; margin-left: 20px;" ></i> -->
         </div><span class=""></span>
-      </div>
-    
-        <div class="overview-boxes"> 
-          <div class="box" style="background-color: aquamarine;">
-            <div class="box-topic">STRAND</div>
-          </div>
-        </div>
-
-      <div class="overview-boxes">
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">HUMMS</div>
-            <div class="number">40,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart-alt cart'></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">GAS</div>
-            <div class="number">38,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-add cart two' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">STEM</div>
-            <div class="number">$12,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart cart three' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">TECHVOC</div>
-            <div class="number">11,086</div>
-            <div class="indicator">
-              <i class='bx bx-down-arrow-alt down'></i>
-              <span class="text">Down From Today</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-download cart four' ></i>
-        </div>
-      </div>
-      
-
-      <div class="sales-boxes">
-        <div class="recent-sales box">
-          <div class="title">Recent Sales</div>
-          <div class="sales-details">
-            <ul class="details">
-              <li class="topic">Date</li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-            </ul>
-            <ul class="details">
-            <li class="topic">Customer</li>
-            <li><a href="#">Alex Doe</a></li>
-            <li><a href="#">David Mart</a></li>
-            <li><a href="#">Roe Parter</a></li>
-            <li><a href="#">Diana Penty</a></li>
-            <li><a href="#">Martin Paw</a></li>
-            <li><a href="#">Doe Alex</a></li>
-            <li><a href="#">Aiana Lexa</a></li>
-            <li><a href="#">Rexel Mags</a></li>
-             <li><a href="#">Tiana Loths</a></li>
-          </ul>
-          <ul class="details">
-            <li class="topic">Sales</li>
-            <li><a href="#">Delivered</a></li>
-            <li><a href="#">Pending</a></li>
-            <li><a href="#">Returned</a></li>
-            <li><a href="#">Delivered</a></li>
-            <li><a href="#">Pending</a></li>
-            <li><a href="#">Returned</a></li>
-            <li><a href="#">Delivered</a></li>
-             <li><a href="#">Pending</a></li>
-            <li><a href="#">Delivered</a></li>
-          </ul>
-          <ul class="details">
-            <li class="topic">Total</li>
-            <li><a href="#">$204.98</a></li>
-            <li><a href="#">$24.55</a></li>
-            <li><a href="#">$25.88</a></li>
-            <li><a href="#">$170.66</a></li>
-            <li><a href="#">$56.56</a></li>
-            <li><a href="#">$44.95</a></li>
-            <li><a href="#">$67.33</a></li>
-             <li><a href="#">$23.53</a></li>
-             <li><a href="#">$46.52</a></li>
-          </ul>
-          </div>
-          <div class="button">
-            <a href="#">See All</a>
-          </div>
-        </div>
-        <div class="top-sales box">
-          <div class="title">Top Seling Product</div>
-          <ul class="top-sales-details">
-            <li>
-            <a href="#">
-              <img src="images/sunglasses.jpg" alt="">
-              <span class="product">Vuitton Sunglasses</span>
-            </a>
-            <span class="price">$1107</span>
-          </li>
-          <li>
-            <a href="#">
-               <img src="images/jeans.jpg" alt="">
-              <span class="product">Hourglass Jeans </span>
-            </a>
-            <span class="price">$1567</span>
-          </li>
-          <li>
-            <a href="#">
-             <img src="images/nike.jpg" alt="">
-              <span class="product">Nike Sport Shoe</span>
-            </a>
-            <span class="price">$1234</span>
-          </li>
-          <li>
-            <a href="#">
-              <img src="images/scarves.jpg" alt="">
-              <span class="product">Hermes Silk Scarves.</span>
-            </a>
-            <span class="price">$2312</span>
-          </li>
-          <li>
-            <a href="#">
-              <img src="images/blueBag.jpg" alt="">
-              <span class="product">Succi Ladies Bag</span>
-            </a>
-            <span class="price">$1456</span>
-          </li>
-          <li>
-            <a href="#">
-              <img src="images/bag.jpg" alt="">
-              <span class="product">Gucci Womens's Bags</span>
-            </a>
-            <span class="price">$2345</span>
-          <li>
-            <a href="#">
-              <img src="images/addidas.jpg" alt="">
-              <span class="product">Addidas Running Shoe</span>
-            </a>
-            <span class="price">$2345</span>
-          </li>
-<li>
-            <a href="#">
-             <img src="images/shirt.jpg" alt="">
-              <span class="product">Bilack Wear's Shirt</span>
-            </a>
-            <span class="price">$1245</span>
-          </li>
-          </ul>
-        </div>
       </div>
     </div>
   </section>
@@ -315,6 +230,7 @@ if (hidden) {
 }
 }
 </script>
+<script type="text/javascript" src=" ../js/analytics_school_dashboard.js"></script>
 
 </body>
 </html>
