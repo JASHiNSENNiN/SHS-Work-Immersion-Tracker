f0f25449069177bb54262f311190c667328040ce
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-03-07 00:41:25 +0800

    Add files via upload

---
+++ php/post_work_Immersion.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Search Portal</title>
+    <link rel="stylesheet" type="text/css" href="../css/post_wi.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <a href="post_work_immersion.php">
+                <img src="../img/companylogo.jpg" alt="Logo">
+            </a>
+            <nav>
+            <a href="post_work_immersion.php">Post Work Immersion</a>
+            <!-- <a href="./php/job_list.php">Find a Work Immerssion</a>
+            <a href="./php/contact.php">Contact</a> -->
+
+        </nav>
+
+        </div>
+       
+        <a class="login-btn" href="#" style="margin-left: 20px;">Sign in</a>
+        
+    </header>
+
+    <section>
+        <h1 class="sfa">Let's Workify Your Work Life</h1>
+        <div class="searchwork">
+            <form action="#" method="get">
+                <!-- <input type="text" name="keywords" placeholder="Immerssion  / Keywords / Company">
+                <input type="text" name="location" placeholder="Location"> -->
+                <input type="submit" value="Post a Work Immersion!">
+
+            </form>
+        </div>
+    </section>
+    <script>
+        document.getElementById("currentDate").innerHTML = new Date().getFullYear();
+    </script>
+
+</body>
+<style>
+
+</style>
+
+</html>
