c7bbce37ef083979771a93fa8349feab6852b9bb
JASHiN <95284497+JASHiNSENNiN@users.noreply.github.com>
2024-03-21 03:10:00 +0800

    0auth testing (no after login redirection yet)

--- backend/php/0Auth/auth_login.php
+++ backend/php/0Auth/auth_login.php
@@ -1,8 +1,6 @@
 <?php require_once '../vendor\autoload.php';
 
-$clientID = '228321911034-th9gclqk8osq7trccfhahtitpjm9972s.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectURL = 'http://localhost/shs/php/login_form.php';
+
 
 // client request sa elgoog
 $client = new Google_Client();
