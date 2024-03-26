78a1fd467831cda3ad882f235bc725b3cbcca757
Leahcim1270 <aldeakel27@gmail.com>
2024-03-26 11:25:25 +0800

    Landing page

---
+++ php/landing_page.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page</title>
+    <!--ICON-->
+<link rel="shortcut icon" href="images/logo.svg">
+
+<!--META TAGS-->
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<!-- <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="author" content="Mahesh">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta property="og:locale" content="en_US" />
+<meta property="og:url" content="" />
+<meta property="og:site_name" content="LEVIOOSA" /> -->
+
+<!--EXTERNAL CSS-->
+<link rel="stylesheet" href="../css/landing-page.css">
+
+
+<!--PLUGIN-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<!--FONT AWESOME-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+<!--GOOGLE FONTS-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Fredoka+One&family=Kanit:wght@300&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <!--NAV-BAR-->
+<header>
+
+<div class="topnav" id="myTopnav">
+  <!-- <a class="logo">  -->
+    
+  <!-- </a> -->
+  <!-- <a href="#team">Sitemap</a>
+  <a href="#team">Team</a>
+  <div class="dropdown">
+    <button class="dropbtn">Services 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+   <a href="/MAHESHBYL/details/mdOmWZG">Contact</a>
+  <a href="about.html">About</a>
+   <a href="#" class="active">Home</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a> -->
+</div>
+</header>
+
+<div id="navbar">
+<div class="slide-menu">
+  <a class="logo">WORKIFY</a>
+  <a href="#team">Sitemap</a>
+  <a href="#team">Team</a>
+  <div class="dropdown">
+    <button class="dropbtn">Services 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+   <a href="https://codepen.io/MAHESHBYL/details/mdOmWZG">Contact</a>
+  <a href="about.html">About</a>
+   <a href="#" class="active">Home</a>
+</div>
+</div>
+
+
+
+<!--MAIN-->
+<div class="main">
+<img style="padding-left: 20px;" src="../img/WORKIFY-LOGO.svg" alt="logo">
+<table>
+<tr>
+<td>
+<section>
+
+<h5 class="sub-title">Welcome to Workify</h5>
+<h1 class="title">CUSTOMIZED WORK IMMERSION TO JUMPSTART YOUR CAREER</h1>
+<p>Get ready to take the first step towards your Work Immersion with Workify. </p>
+<a href="../index.php" class="btn1">Get Start</a>
+</section>
+</td>
+<td>
+<img src="../img/landing-page.svg" class="inline-photo show-on-scroll" alt="INFERNO">
+</td>
+</tr>
+</table>
+</div>
+
+
+<!--CARDS-->
+<section class="section0" id="sections">
+<div class="card banner">
+<h1 class="title">Our Features</h1>
+<h5 class="sub-title">Our platform streamlines all aspects of work immersion management, from organizing student placements to tracking progress and evaluating outcomes.</h5>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-database"></i>
+<h3>Data Collection and Integration</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-tachometer"></i>
+<h3>Dashboard and Reporting</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-map-marker"></i>
+<h3>Matching and Placement</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-check"></i>
+<h3>Performance Evaluation</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-industry"></i>
+<h3>Identifying Industry Trends</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-file"></i>
+<h3>Feedback and Surveys</h3>
+</div>
+<div class="card inline-photo show-on-scroll">
+<i class="fa fa-exclamation-triangle"></i>
+<h3>Risk Assessment and Intervention</h3>
+</div>
+</section>
+
+
+<!--CONTAINER-->
+<div class="container">
+<table>
+<tr>
+<td>
+<img src="https://i.ibb.co/w4dySWD/01.webp" class="inline-photo show-on-scroll" alt="About Us">
+</td>
+<td>
+<h6 class="sub-title">Discover Our Website</h6>
+<h1 class="title">Empower Your Career with Workify</h1>
+<p>At workify, we understand that finding work immersion opportunities can be a challenge for schools and students alike. That’s why we’ve created a platform that connects schools with our partner industries, making it easier than ever before to find the perfect opportunity. Our goal is to provide students with real-world experiences that give them the skills and knowledge they need to pursue their careers with confidence.</p>
+<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+<a href="#moreaboutus" class="btn2">Discover More</a>
+</td>
+</tr>
+</table>
+</div>
+
+
+<!-- 
+<section class="slick" id="moreaboutus">
+<div class="banner">
+	<h1 class="title">How You Can Benifit </h1>
+	<h5 class="sub-title">Bringing New IT Business solutions and ideas</h5>
+	</div>
+	<table>
+	<tr>
+		<td>
+		<ul>
+		<li>
+		<h3>Discovery phase </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+		<h3>Effective development methodologies </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		<li>
+		<h3>Favorable timezone and cultural closeness </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		</ul>
+		</td>
+		<td>
+		<div class="card inline-photo show-on-scroll">
+        <div class="card_part card_part-one">
+        </div> -->
+  
+       <!-- Photo 2 -->
+       <!-- <div class="card_part card_part-two">
+       </div> -->
+
+       <!-- Photo 3 -->
+       <!-- <div class="card_part card_part-three">
+       </div> -->
+
+       <!-- Photo 4 -->
+       <!-- <div class="card_part card_part-four">
+       </div>
+       </div> -->
+		<!-- </td>
+		<td>
+		<ul>
+		<li>
+		<h3>Centres of Excellence </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</li>
+		<li>
+		<h3>Clear, timely communication </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</li>
+		<li>
+		<h3>Quick and easy start </h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+		</li>
+		</ul>
+		</td>
+	</tr>
+	</table>
+	</section> -->
+	
+	
+
+
+
+<!-- <section class="section1">
+<div class="banner">
+	<h1 class="title">Technologies & Frameworks Our Teams Skilled In</h1>
+	<h5 class="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h5>
+	</div>
+<button class="accordion">Question-1</button>
+<div class="panel">
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+</div>
+
+<button class="accordion">Question-2</button>
+<div class="panel">
+ <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+</div>
+
+<button class="accordion">Question-3</button>
+<div class="panel">
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+</div>
+
+<button class="accordion">Question-4</button>
+<div class="panel">
+   <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+</div>
+
+</section> -->
+	
+	
+	
+<!--FOOTER-->
+<footer>
+<!-- <div class="footer">
+<section>
+<p><img src="img/logo.png" alt="LEVIOOSA"></p>
+<p> <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p></p>
+</section>
+
+<section>
+<p class="title">OTHER LINKS</p>
+<p><a href="#">Ticket</a></p>
+<p><a href="#">Terms & Conditions</a></p>
+<p><a href="#">Cookie Policy</a></p>
+<p><a href="#">Privacy Policy</a></p>
+</section>
+
+<section>
+<p class="title">SHORT CUT</p>
+<p><a href="#">Contact Us</a></p>
+<p><a href="#">Services</a></p>
+<p><a href="#">About Us</a></p>
+<p><a href="#">Our Fields</a></p>
+</section>
+
+<section>
+<p class="title">News Letter</p>
+<p><input type="email"placeholder="Email*" required><button type="submit"><i class="fa fa-paper-plane"></i></button></p>
+<p><a title="Address, Country, Pincode"><i class="fa fa-map-marker"></i></a> <a href="#"><i class="fa fa-phone"></i></a> <a href="#"><i class="fa fa-envelope"></i></a></p>
+</section>
+</div> -->
+<div class="sub-footer">
+2024 Your Website. All rights reserved. | Junior Philippines Computer Society Students
+</div>
+</footer>
+
+
+<a href="#" id="scroll-up"><i class="fa fa-angle-up"></i></a>
+</div>
+
+<!--JAVASCRIPT-->
+<script src="../js/landing-page.js"></script>
+
+  
+  <!-- <div class="credits">W3SCHOOLS, UNSPLASH, STACKOVERFLOW</div> -->
+
+    
+</body>
+</html>
