963f248cf83197e482fe0fc8f2b037ea6996266f
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-22 19:50:21 +0800

    login form concatenation fix

--- php/login_form.php
+++ php/login_form.php
@@ -39,24 +39,31 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/backend/php/recaptcha.php';
 
 
                 <!-- --------------------------------Line OR--------------------------->
-                <form id="loginForm" method="POST" onsubmit="return validateForm()" action="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/backend/php/recaptcha.php' ?>">
+                <form id="loginForm" method="POST" onsubmit="">
                     <input type="hidden" name="login_form" value="login_form">
                     <div class="dd-privacy-allow css-e1gwqt e15p7aqh1"><span class="css-8u2krs esbq1260">
                             <span role="separator" aria-orientation="horizontal">&nbsp;</span></span>
                         <div class="css-1lfd96m e15p7aqh0"><span class="css-sfm6zc e1wnkr790">or </span></div>
                     </div>
-                    <label class="css-ddheu4"> Email address <span aria-hidden="true" class="css-ers2ar es2vvo71">&nbsp;*</span> </label>
+                    <label class="css-ddheu4"> Email address <span aria-hidden="true"
+                            class="css-ers2ar es2vvo71">&nbsp;*</span> </label>
                     <input autocomplete="email" type="text" placeholder="" id="login-email" name="login_email" required>
 
-                    <label class="css-ddheu4"> Password <span aria-hidden="true" class="css-ers2ar es2vvo71">&nbsp;*</span>
+                    <label class="css-ddheu4"> Password <span aria-hidden="true"
+                            class="css-ers2ar es2vvo71">&nbsp;*</span>
                     </label>
-                    <input autocomplete="current-password" type="password" placeholder="" id="login-password" name="login_password" required>
-                    <button class="g-recaptcha btn-login" data-sitekey="6Lfa9MIpAAAAALAoYvFEZ86D6SvXCMeXjJ1ULag6" data-callback='onSubmit' data-action='login_form_submit' style="height: 40px; font-size: 15px">
-                        <span class="hover-underline-animation"> Continue </span>
-                        <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16" fill="#fff">
-                            <path id="Path_10" data-name="Path 10" form d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-                            g>
+                    <input autocomplete="current-password" type="password" placeholder="" id="login-password"
+                        name="login_password" required>
+                    <button class="g-recaptcha btn-login" data-sitekey="6Lfa9MIpAAAAALAoYvFEZ86D6SvXCMeXjJ1ULag6"
+                        data-callback='onSubmit' data-action='login_form_submit' style="height: 40px; font-size: 15px">
+                        <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10"
+                            viewBox="0 0 46 16" fill="#fff">
+                            <path id="Path_10" data-name="Path 10"
+                                d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                                transform="translate(30)"></path>
+                        </svg>
                     </button>
+
                 </form>
 
                 <!-- --------------------------------Line OR--------------------------->
@@ -83,20 +90,32 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/backend/php/recaptcha.php';
                         Register with Google
                     </button></a>
 
-                <form id="registerForm" action="one_time_password.php" method="POST" onsubmit="return validateRegisterForm()">
+                <form id="registerForm" action="one_time_password.php" method="POST"
+                    onsubmit="return validateRegisterForm()"
+                    action="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>/backend/php/recaptcha.php">
                     <input type="hidden" name="register_form" value="register_form">
                     <div class="dd-privacy-allow css-e1gwqt e15p7aqh1"><span class="css-8u2krs esbq1260">
                             <span role="separator" aria-orientation="horizontal">&nbsp;</span></span>
                         <div class="css-1lfd96m e15p7aqh0"><span class="css-sfm6zc e1wnkr790">or </span></div>
                     </div>
-                    <input type="text" for="email" name="register_email" id="email" placeholder="Email address" required>
+                    <input type="text" for="email" name="register_email" id="email" placeholder="Email address"
+                        required>
                     <input type="password" placeholder="Password" id="password" name="register_password" required>
-                    <input type="password" placeholder="Confirm Password" id="confirm-password" name="register_confirm_password" required>
-                    <button class="g-recaptcha btn-login" data-sitekey="6Lfa9MIpAAAAALAoYvFEZ86D6SvXCMeXjJ1ULag6" data-callback='onSubmit' data-action='register_form_submit' type="submit">Register</button>
-                    <button class="btn-new" id="switch-to-login" onclick="showLoginForm()">
-                        Log in to Existing Account
-                    </button>
+                    <input type="password" placeholder="Confirm Password" id="confirm-password" ata
+                        name="register_confirm_password" required>
+                    <button type="submit" class="g-recaptcha btn-login"
+                        data-sitekey="6Lfa9MIpAAAAALAoYvFEZ86D6SvXCMeXjJ1ULag6" data-callback='onSubmit'
+                        data-action='register_form_submit'>Register</button>
                 </form>
+
+                <div class="dd-privacy-allow css-e1gwqt e15p7aqh1"><span class="css-8u2krs esbq1260">
+                        <span role="separator" aria-orientation="horizontal"></span></span>
+
+                </div>
+
+                <button class="btn-new" id="switch-to-login" onclick="showLoginForm()">
+                    Log in to Existing Account
+                </button>
             </div>
         </div>
 
@@ -111,8 +130,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/backend/php/recaptcha.php';
 
 </html>
 <script>
-    function onSubmit(token) {
-        document.getElementById("loginForm").submit();
-        document.getElementById("registerForm").submit();
-    }
+function onSubmit(token) {
+    document.getElementById("loginForm").submit();
+    document.getElementById("registerForm").submit();
+}
 </script>
