479d858659d191757475f0b3b5f979e7f50e48af
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-24 03:26:24 +0800

    account registration fix

--- backend/php/account_registration.php
+++ backend/php/account_registration.php
@@ -6,8 +6,8 @@ $password = "@--Workify000";
 $database = "u487450272_shs_immersion";
 $conn = mysqli_connect($host, $username, $password, $database);
 
-$stmt = $conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-$stmt->bind_param("sss", $_SESSION['email'], $_SESSION['password']);
+$stmt = $conn->prepare("INSERT INTO users (email, password, account_type) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $_SESSION['email'], $_SESSION['password'], '');
 $stmt->execute();
 
 $stmt->close();
