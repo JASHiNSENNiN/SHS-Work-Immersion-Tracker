40c47c1c3eb66dab134b6f90cd75d26e79ce0e75
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-24 03:23:18 +0800

    added account registration database connection

--- backend/php/account_registration.php
+++ backend/php/account_registration.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . '/backend/php/config.php';
+$host = "localhost";
+$username = "u487450272_workify_admin";
+$password = "@--Workify000";
+$database = "u487450272_shs_immersion";
+$conn = mysqli_connect($host, $username, $password, $database);
 
 $stmt = $conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
 $stmt->bind_param("sss", $_SESSION['email'], $_SESSION['password']);
