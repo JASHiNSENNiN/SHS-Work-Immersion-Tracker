0a33fcf33edd0de35d3ea91253bb1c0e864a9467
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-23 20:50:49 +0800

    recaptcha score 1.0 test

--- backend/php/recaptcha/registerRecaptcha.php
+++ backend/php/recaptcha/registerRecaptcha.php
@@ -17,7 +17,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
 $arrResponse = json_decode($response, true);
-if ($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+if ($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 1.0) {
     $_SESSION['register_email'] = $_POST['register_email'];
     $_SESSION['register_password'] = $_POST['register_password'];
     $destination = $_SERVER['DOCUMENT_ROOT'] . 'one_time_password.php';
