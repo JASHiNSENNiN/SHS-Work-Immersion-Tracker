d1e8ea00e827d74a4cf27d19cc1405a8cd1dd56d
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-04-24 15:30:18 +0800

    back

--- get_started.php
+++ get_started.php
@@ -89,8 +89,7 @@ session_start();
                         <nav>
                             <a href="login_form.php"><button class="btn-login" id="switch-to-login" onclick="showLoginForm()">
 
-                                    <p>
-                                        << Back</p>
+                                    <p>Back</p>
                                 </button></a>
                             <button class="btn-new" onclick="validateForm()">
                                 <p>Submit</p>
