b067440d9aec2f115d7e398897aaee75984afee6
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-24 18:30:49 +0800

    checking for duplicate

--- register.php
+++ register.php
@@ -41,7 +41,8 @@
                         Register with Google
                     </button></a>
 
-                <form id="registerForm" method="POST" onsubmit="return validateRegisterForm()">
+                <form id="registerForm" method="POST"
+                    onsubmit="return validateRegisterForm(<?php echo checkDuplicateEmail() ?>)">
                     <div class="dd-privacy-allow css-e1gwqt e15p7aqh1"><span class="css-8u2krs esbq1260">
                             <span role="separator" aria-orientation="horizontal">&nbsp;</span></span>
                         <div class="css-1lfd96m e15p7aqh0"><span class="css-sfm6zc e1wnkr790">or </span></div>
