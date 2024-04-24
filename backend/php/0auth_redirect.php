b0315e93a030eec8d800f13d07e8c0544e0e16de
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-25 02:44:30 +0800

    auth redirect handler update

--- backend/php/0auth_redirect.php
+++ backend/php/0auth_redirect.php
@@ -5,9 +5,16 @@ require_once $_SERVER['DOCUMENT_ROOT']
 
 
 if (checkDuplicateEmail($_SESSION['email']) == true) {
-    $destination = 'https://www.workifyph.online/login.php?error=0AuthDuplicateEmail';
-    header("Location: $destination");
-    exit();
+    if (checkEmailPass() === true) {
+        $destination = 'https://www.workifyph.online/login.php?error=0AuthDuplicateEmail';
+        header("Location: $destination");
+        exit();
+    }
+    if (checkAccType() === false) {
+        $destination = 'https://www.workifyph.online/get_started.php';
+        header("Location: $destination");
+        exit();
+    }
 }
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
@@ -24,3 +31,58 @@ $stmt->bind_param("s", $_SESSION['email']);
 $stmt->execute();
 $stmt->close();
 $conn->close();
+$destination = 'https://www.workifyph.online/get_started.php';
+header("Location: $destination");
+exit();
+
+function checkEmailPass()
+{
+    $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+    $dotenv->load();
+
+    $host = "localhost";
+    $username = $_ENV['MYSQL_USERNAME'];
+    $password = $_ENV['MYSQL_PASSWORD'];
+    $database = $_ENV['MYSQL_DBNAME'];
+
+    $conn = new mysqli($host, $username, $password, $database);
+    $stmt = $conn->prepare("SELECT password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $_SESSION['email']);
+    $stmt->execute();
+    $stmt->bind_result($password);
+    $stmt->fetch();
+    $stmt->close();
+    $conn->close();
+
+    if ($password) {
+        return true; //has pw
+    } else {
+        return false;
+    }
+}
+
+function checkAccType()
+{
+    $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+    $dotenv->load();
+
+    $host = "localhost";
+    $username = $_ENV['MYSQL_USERNAME'];
+    $password = $_ENV['MYSQL_PASSWORD'];
+    $database = $_ENV['MYSQL_DBNAME'];
+
+    $conn = new mysqli($host, $username, $password, $database);
+    $stmt = $conn->prepare("SELECT account_type FROM users WHERE email = ?");
+    $stmt->bind_param("s", $_SESSION['email']);
+    $stmt->execute();
+    $stmt->bind_result($password);
+    $stmt->fetch();
+    $stmt->close();
+    $conn->close();
+
+    if ($password) {
+        return true; //has at
+    } else {
+        return false;
+    }
+}
