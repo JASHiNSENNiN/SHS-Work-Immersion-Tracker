3bccdae4a60cd194bfc75a8527faaba2cd0166ae
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-04-24 15:15:37 +0800

    change icon back and gaq

--- get_started.php
+++ get_started.php
@@ -88,10 +88,9 @@ session_start();
                         </div>
                         <nav>
                             <a href="login_form.php"><button class="btn-login" id="switch-to-login" onclick="showLoginForm()">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                                        <path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" data-name="4-Arrow Left" />
-                                    </svg>
-                                    <p> Back</p>
+
+                                    <p>
+                                        << Back</p>
                                 </button></a>
                             <button class="btn-new" onclick="validateForm()">
                                 <p>Submit</p>
