d494b25250b6e66cccb68c2334c8a34da53d151b
JASHiNSENNiN <jacercinense21@gmail.com>
2024-04-25 02:59:58 +0800

    fix 0auth logic

--- backend/php/0auth_redirect.php
+++ backend/php/0auth_redirect.php
@@ -10,20 +10,33 @@ if (checkDuplicateEmail($_SESSION['email']) == true) {
         $destination = 'https://www.workifyph.online/login.php?error=0AuthDuplicateEmail';
         header("Location: $destination");
         exit();
+    } else {
+    }
+    if (checkAccType() === false) {
+        $destination = 'https://www.workifyph.online/get_started.php';
+        header("Location: $destination");
+        exit();
+    }
+} else {
+    $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+    $dotenv->load();
+
+    $host = "localhost";
+    $username = $_ENV['MYSQL_USERNAME'];
+    $password = $_ENV['MYSQL_PASSWORD'];
+    $database = $_ENV['MYSQL_DBNAME'];
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    $stmt = $conn->prepare("INSERT INTO users (email) VALUES (?)");
+    $stmt->bind_param("s", $_SESSION['email']);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    if (checkAccType() === false) {
+        $destination = 'https://www.workifyph.online/get_started.php';
+        header("Location: $destination");
+        exit();
+    } else {
     }
 }
-$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
-$dotenv->load();
-
-$host = "localhost";
-$username = $_ENV['MYSQL_USERNAME'];
-$password = $_ENV['MYSQL_PASSWORD'];
-$database = $_ENV['MYSQL_DBNAME'];
-
-$conn = new mysqli($host, $username, $password, $database);
-
-$stmt = $conn->prepare("INSERT INTO users (email) VALUES (?)");
-$stmt->bind_param("s", $_SESSION['email']);
-$stmt->execute();
-$stmt->close();
-$conn->close();
