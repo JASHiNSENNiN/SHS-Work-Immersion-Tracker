238c0233dda050664f450ca941b019743c3d190b
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-05-08 23:07:44 +0800

    snaqshot

--- php/Company/Details.php
+++ php/Company/Details.php
@@ -106,37 +106,63 @@
 
             <div class="api_card">
                 <div class="api_details">
-                    <p class="api_title">Ceo</p></br>
-                    <p class="api_details_one">Robert Victor G. Seares, Jr.</p></br>
-                    <p class="api_details_two">Earnings: 123456</p></br>
-                    <p class="api_details_three">Earnings: 123456</p>
+                    <span class="api_title">Ceo</span></br>
+                    <span class="api_details_one">Robert Victor G. Seares, Jr.</span></br>
+                    <div class="rating">62%</div></br>
+                    <span class="api_details_two">approve of Robert Victor G. Seares, Jr. performance</span></br>
+
+
                 </div>
 
             </div>
-            <div class="api_card">
+            <!-- <div class="api_card">
                 <div class="api_details">
-                    <p class="api_title">Paystub</p></br>
-                    <p class="api_details_one">Payday: 02/01/2021</p></br>
-                    <p class="api_details_two">Earnings: 123456</p></br>
-                    <p class="api_details_three">Earnings: 123456</p>
+                    <span class="api_title">Headerquarters</span></br>
+                    <span class="api_details_one">Payday: 02/01/2021</span></br>
+
+                </div>
+
+            </div> -->
+            <div class="section-wrap">
+                <div class="api_card_half">
+                    <div class="api_details">
+                        <p class="api_title">Headerquarters</p></br>
+                        <span class="api_details_one">Quezon City, Metro Manila</span>
+                    </div>
+
                 </div>
 
             </div>
             <div class="section-wrap">
                 <div class="api_card_half">
                     <div class="api_details">
-                        <p class="api_title">Paystub</p></br>
+                        <p class="api_title">Link</p></br>
+                        <a class="api_details_one" style="color: #2557a7;" href="https://www.nia.gov.ph/">NIA Philippines <svg xmlns="http://www.w3.org/2000/svg" focusable="false" role="img" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" class=" css-1jtd2m7 eac13zx0">
+                                <title>Opens in a new window</title>
+                                <path d="M14.504 3a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h3.085l-9.594 9.594a.5.5 0 000 .707l.707.708a.5.5 0 00.707 0l9.594-9.595V9.5a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-6a.5.5 0 00-.5-.5h-6z"></path>
+                                <path d="M5 3.002a2 2 0 00-2 2v13.996a2 2 0 001.996 2.004h14a2 2 0 002-2v-6.5a.5.5 0 00-.5-.5h-1a.5.5 0 00-.5.5v6.5L5 18.998V5.002L11.5 5a.495.495 0 00.496-.498v-1a.5.5 0 00-.5-.5H5z"></path>
+                            </svg></a>
                     </div>
 
                 </div>
-                <div class="api_card_half">
-                    <div class="api_details">
-                        <p class="api_title">Paystub</p></br>
-                    </div>
 
-                </div>
             </div>
         </div>
+        <!-- <h1>What is CSS?</h1>
+        Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language.Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications.
+        <div>
+            <br>
+            <span id="othertext">CSS is designed primarily to enable the separation of document content from document presentation, including aspects such as the layout, colors, and fonts. This separation can improve content accessibility, provide more flexibility and control in the specification of presentation characteristics, enable multiple HTML pages to share formatting by specifying the relevant CSS in a separate .css file, and reduce complexity and repetition in the structural content.
+                Separation of formatting and content makes it possible to present the same markup page in different styles for different rendering methods, such as on-screen, in print, by voice (via speech-based browser or screen reader), and on Braille-based tactile devices. It can also display the web page differently depending on the screen size or viewing device. Readers can also specify a different style sheet, such as a CSS file stored on their own computer, to override the one the author specified.
+                Changes to the graphic design of a document (or hundreds of documents) can be applied quickly and easily, by editing a few lines in the CSS file they use, rather than by changing markup in the documents.
+                The CSS specification describes a priority scheme to determine which style rules apply if more than one rule matches against a particular element. In this so-called cascade, priorities (or weights) are calculated and assigned to rules, so that the results are predictable.
+                The CSS specifications are maintained by the World Wide Web Consortium (W3C). Internet media type (MIME type) text/css is registered for use with CSS by RFC 2318 (March 1998). The W3C operates a free CSS validation service for CSS documents.<br>
+
+            </span>
+            <div class="btn-container">
+                <button class="btn-descriqtion" id="togglebtn">Read More</button>
+            </div>
+        </div> -->
 
     </div>
     <br>
@@ -167,3 +193,38 @@
 </body>
 
 </html>
+<script>
+    /*
+Conic gradients are not supported in all browsers (https://caniuse.com/#feat=css-conic-gradients), so this pen includes the CSS conic-gradient() polyfill by Lea Verou (https://leaverou.github.io/conic-gradient/)
+*/
+
+    // Find al rating items
+    const ratings = document.querySelectorAll(".rating");
+
+    // Iterate over all rating items
+    ratings.forEach((rating) => {
+        // Get content and get score as an int
+        const ratingContent = rating.innerHTML;
+        const ratingScore = parseInt(ratingContent, 10);
+
+        // Define if the score is good, meh or bad according to its value
+        const scoreClass =
+            ratingScore < 40 ? "bad" : ratingScore < 60 ? "meh" : "good";
+
+        // Add score class to the rating
+        rating.classList.add(scoreClass);
+
+        // After adding the class, get its color
+        const ratingColor = window.getComputedStyle(rating).backgroundColor;
+
+        // Define the background gradient according to the score and color
+        const gradient = `background: conic-gradient(${ratingColor} ${ratingScore}%, transparent 0 100%)`;
+
+        // Set the gradient as the rating background
+        rating.setAttribute("style", gradient);
+
+        // Wrap the content in a tag to show it above the pseudo element that masks the bar
+        rating.innerHTML = `<span>${ratingScore} ${
+    ratingContent.indexOf("%") >= 0 ? "<small>%</small>" : ""}</span>`;
+    });
+</script>
