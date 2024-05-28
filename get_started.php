f4c0c920fa76e0d515a831cb7256eca4e340a95d
JASHiNSENNiN <jacercinense21@gmail.com>
2024-05-28 16:02:01 +0800

    form update

--- get_started.php
+++ get_started.php
@@ -55,11 +55,11 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
 
 <head>
     <script>
-        window.onload = function() {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', '/backend/php/ajax/checkAccType.php', true);
-            xhr.send();
-        };
+    window.onload = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/backend/php/ajax/checkAccType.php', true);
+        xhr.send();
+    };
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -74,15 +74,17 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
     <script src="/backend/js/register.js"></script>
     <script src="/js/get_start_log.js"></script>
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 </head>
 
 <body>
     <noscript>
         <style>
-            html {
-                display: none;
-            }
+        html {
+            display: none;
+        }
         </style>
         <meta http-equiv="refresh" content="0.0;url=https://www.workifyph.online/message.php">
     </noscript>
@@ -106,24 +108,27 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
                     <div class="form-container">
                         <form id="setupForm" method="POST" onsubmit="return validateSetupForm()">
 
-                            <input type="text" for="email" name="email" id="email" placeholder="<?php echo $_SESSION['email'] ?>" value="<?php echo $_SESSION['email'] ?>" disabled>
+                            <input type="text" for="email" name="email" id="email"
+                                placeholder="<?php echo $_SESSION['email'] ?>" value="<?php echo $_SESSION['email'] ?>"
+                                disabled>
                             <select id="account-type" name="account-type" onchange="toggleFields()" required>
-                                <option value class="null-type">Account Type:</option>
+                                <option value="" selected disabled hidden class="null-type">Account Type:</option>
                                 <option value="student">Student</option>
                                 <option value="school">School</option>
                                 <option value="organization">Partner Organization</option>
                             </select>
                             <div id="student-fields" style="display: none;">
-                                <input type="text" placeholder="First Name" id="first-name" name="first-name">
-                                <input type="text" placeholder="Middle Name" id="middle-name" name="middle-name">
-                                <input type="text" placeholder="Last Name" id="last-name" name="last-name">
+                                <input value="" type="text" placeholder="First Name" id="first-name" name="first-name">
+                                <input value="" type="text" placeholder="Middle Name" id="middle-name"
+                                    name="middle-name">
+                                <input value="" type="text" placeholder="Last Name" id="last-name" name="last-name">
                                 <select name="grade-level" id="grade-level">
                                     <option value class="null-type">Grade Level:</option>
                                     <option value="11">11</option>
                                     <option value="12">12</option>
                                 </select>
                                 <select name="strand" id="strand">
-                                    <option value class="null-type">Strand:</option>
+                                    <option value="" selected disabled hidden class="null-type">Strand:</option>
                                     <option value="stem">STEM</option>
                                     <option value="humss">HUMSS</option>
                                     <option value="abm">ABM</option>
@@ -132,12 +137,14 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
                                 </select>
                             </div>
                             <div id="school-fields" style="display: none;">
-                                <input type="text" placeholder="School Name" id="school-name" name="school-name">
+                                <input value="" type="text" placeholder="School Name" id="school-name"
+                                    name="school-name">
                             </div>
                             <div id="partner-fields" style="display: none;">
-                                <input type="text" placeholder="Organization Name" id="organization-name" name="organization-name">
+                                <input value="" type="text" placeholder="Organization Name" id="organization-name"
+                                    name="organization-name">
                                 <select name="strand-focus" id="strand-focus">
-                                    <option value class="null-type">Strand:</option>
+                                    <option value="" selected disabled hidden class="null-type">Strand:</option>
                                     <option value="stem">STEM</option>
                                     <option value="humss">HUMSS</option>
                                     <option value="abm">ABM</option>
@@ -151,10 +158,12 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
 
                                         <p>Back</p>
                                     </button></a>
-                                <button class="btn-new" data-sitekey="6Lfa9MIpAAAAALAoYvFEZ86D6SvXCMeXjJ1ULag6" data-callback="onSubmit" data-action="submit" style="right: 177px;">
+                                <button class="btn-new" style="right: 177px;">
                                     <p>Submit</p>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                                        <path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right" />
+                                        <path
+                                            d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z"
+                                            data-name="3-Arrow Right" />
                                     </svg>
                                 </button>
 
@@ -173,34 +182,34 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
     </div>
 </body>
 <script>
-    function toggleFields() {
-        var accountType = document.getElementById("account-type").value;
-        var studentFields = document.getElementById("student-fields");
-        var schoolFields = document.getElementById("school-fields");
-        var partnerFields = document.getElementById("partner-fields");
-        var registerForm = document.getElementById("register-form");
+function toggleFields() {
+    var accountType = document.getElementById("account-type").value;
+    var studentFields = document.getElementById("student-fields");
+    var schoolFields = document.getElementById("school-fields");
+    var partnerFields = document.getElementById("partner-fields");
+    var registerForm = document.getElementById("register-form");
 
-        if (accountType === "student") {
-            registerForm.style.paddingTop = "25%";
-            studentFields.style.display = "block";
-            schoolFields.style.display = "none";
-            partnerFields.style.display = "none";
-        } else if (accountType === "school") {
-            registerForm.style.paddingTop = "10%";
-            studentFields.style.display = "none";
-            schoolFields.style.display = "block";
-            partnerFields.style.display = "none";
-        } else if (accountType === "organization") {
-            registerForm.style.paddingTop = "10%";
-            studentFields.style.display = "none";
-            schoolFields.style.display = "none";
-            partnerFields.style.display = "block";
-        } else {
-            studentFields.style.display = "none";
-            schoolFields.style.display = "none";
-            partnerFields.style.display = "none";
-        }
+    if (accountType === "student") {
+        registerForm.style.paddingTop = "25%";
+        studentFields.style.display = "block";
+        schoolFields.style.display = "none";
+        partnerFields.style.display = "none";
+    } else if (accountType === "school") {
+        registerForm.style.paddingTop = "10%";
+        studentFields.style.display = "none";
+        schoolFields.style.display = "block";
+        partnerFields.style.display = "none";
+    } else if (accountType === "organization") {
+        registerForm.style.paddingTop = "10%";
+        studentFields.style.display = "none";
+        schoolFields.style.display = "none";
+        partnerFields.style.display = "block";
+    } else {
+        studentFields.style.display = "none";
+        schoolFields.style.display = "none";
+        partnerFields.style.display = "none";
     }
+}
 </script>
 
 </html>
