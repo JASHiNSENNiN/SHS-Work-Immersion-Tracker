daa78dd5ae994ea2f1b2f96d387fd98937005b45
Leahcim1270 <159026736+Leahcim1270@users.noreply.github.com>
2024-05-29 22:57:16 +0800

    done 101

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
+    <title>Log in| Workify</title>
     <link rel="stylesheet" type="text/css" href="../css/header.css">
     <link rel="stylesheet" type="text/css" href="../css/loginform.css">
     <script src="https://www.google.com/recaptcha/api.js"></script>
